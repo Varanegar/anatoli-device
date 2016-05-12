4039fe2b59cfcff130325754831696df64a97690
ATP at home <aliasghar.toraby@gmail.com>
2016-05-12 15:36:03 +0430

    orders update

--- Source/Device/AnatoliIOS/AnatoliIOS/ViewControllers/OrdersStatusViewController.cs
+++ Source/Device/AnatoliIOS/AnatoliIOS/ViewControllers/OrdersStatusViewController.cs
@@ -3,37 +3,68 @@
 using UIKit;
 using AnatoliIOS.TableViewSources;
 using Anatoli.App.Manager;
+using AnatoliIOS.Components;
+using Anatoli.Framework.AnatoliBase;
 
 namespace AnatoliIOS.ViewControllers
 {
-	public partial class OrdersStatusViewController : BaseController
-	{
-		public OrdersStatusViewController () : base ("OrdersStatusViewController", null)
-		{
-		}
+    public partial class OrdersStatusViewController : BaseController
+    {
+        public OrdersStatusViewController() : base("OrdersStatusViewController", null)
+        {
+        }
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
             EdgesForExtendedLayout = UIRectEdge.None;
         }
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			base.DidReceiveMemoryWarning ();
-			// Release any cached data, images, etc that aren't in use.
-		}
-		public async override void ViewDidAppear (bool animated)
-		{
-			base.ViewDidAppear (animated);
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+        public async override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
             Title = "سفارشات قبلی";
-            var source = new OrderStatusTableViewSource ();
-			source.SetDataQuery(OrderManager.GetOrderQueryString());
-			await source.RefreshAsync ();
-			ordersTableView.Source = source;
-			ordersTableView.ReloadData ();
-		}
-	}
+            if (AnatoliClient.GetInstance().WebClient.IsOnline())
+            {
+                LoadingOverlay loading = new LoadingOverlay(View.Bounds);
+                View.AddSubview(loading);
+                try
+                {
+                    await OrderManager.SyncOrdersAsync(AnatoliApp.GetInstance().Customer.UniqueId);
+                }
+                catch (AnatoliWebClientException ex)
+                {
+                    var alert = new UIAlertView("", ex.MetaInfo.ModelStateString, null, "باشه");
+                    alert.Show();
+                }
+                catch (Exception)
+                {
+                    var alert = new UIAlertView("", "خطا در اجرای درخواست شما", null, "باشه");
+                    alert.Show();
+                }
+                finally
+                {
+                    loading.Hide();
+                }
+            }
+            else
+            {
+                var alert = new UIAlertView("", "برای دریافت آخرین اطلاعات سفارشات خود باید به اینترنت متصل باشید", null, "باشه");
+                alert.Show();
+            }
+
+            var source = new OrderStatusTableViewSource();
+            source.SetDataQuery(OrderManager.GetOrderQueryString());
+            await source.RefreshAsync();
+            ordersTableView.Source = source;
+            ordersTableView.ReloadData();
+        }
+    }
 }
 
 
