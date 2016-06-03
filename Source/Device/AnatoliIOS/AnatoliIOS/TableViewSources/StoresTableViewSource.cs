612c4311e501c7c80393d3342f789831013cad75
ATP on mac <aliasghar.toraby@gmail.com>
2016-06-04 01:19:45 +0430

    ui update

--- Source/Device/AnatoliIOS/AnatoliIOS/TableViewSources/StoresTableViewSource.cs
+++ Source/Device/AnatoliIOS/AnatoliIOS/TableViewSources/StoresTableViewSource.cs
@@ -7,42 +7,56 @@ using UIKit;
 
 namespace AnatoliIOS.TableViewSources
 {
-    public class StoresTableViewSource : BaseTableViewSource<StoreManager, StoreDataModel>
-    {
-        public StoresTableViewSource()
-        {
-        }
-        public override UIKit.UITableViewCell GetCellView(UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
-        {
-            var cell = tableView.DequeueReusableCell(StoreSummaryTableViewCell.Key) as StoreSummaryTableViewCell;
-            cell.UpdateCell(Items[indexPath.Row]);
-            return cell;
-        }
+	public class StoresTableViewSource : BaseTableViewSource<StoreManager, StoreDataModel>
+	{
+		public StoresTableViewSource ()
+		{
+		}
 
-        public async override void RowSelected(UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
-        {
-            var item = Items[indexPath.Row];
-            if (item != null)
-            {
-                if (!item.support_app_order)
-                {
-                    var alert = new UIAlertView("", "این فروشگاه امکان فروش اینترنتی را ندارد", null, "باشه");
-                    alert.Show();
-                    return;
-                }
-                var result = await StoreManager.SelectAsync(item);
-                if (result)
-                {
-                    AnatoliApp.GetInstance().DefaultStore = item;
-                    AnatoliApp.GetInstance().ReplaceViewController(new FirstPageViewController());
-                }
-            }
+		public override UIKit.UITableViewCell GetCellView (UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			var cell = tableView.DequeueReusableCell (StoreSummaryTableViewCell.Key) as StoreSummaryTableViewCell;
+			cell.UpdateCell (Items [indexPath.Row]);
+			return cell;
+		}
 
-        }
-        public override nfloat GetHeightForRow(UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
-        {
-            return 70f;
-        }
-    }
+		public async override void RowSelected (UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			var item = Items [indexPath.Row];
+			if (item != null) {
+				if (!item.support_app_order) {
+					var alert = new UIAlertView ("", "این فروشگاه امکان فروش اینترنتی را ندارد", null, "باشه");
+					alert.Show ();
+					return;
+				}
+				if ((await ShoppingCardManager.GetInfoAsync ()).items_count > 0) {
+					var alert = UIAlertController.Create ("اخطار", ".با تغییر فروشگاه سبد خرید شما خالی میشود. ادامه می دهید؟", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("بی خیال", UIAlertActionStyle.Cancel, null));
+					alert.AddAction (UIAlertAction.Create ("باشه", UIAlertActionStyle.Default,
+						async delegate {
+							var result = await StoreManager.SelectAsync (item);
+							if (result) {
+								AnatoliApp.GetInstance ().DefaultStore = item;
+								AnatoliApp.GetInstance ().ReplaceViewController (new FirstPageViewController ());
+							}
+						}));
+					AnatoliApp.GetInstance ().PresentViewController (alert);
+				} else {
+					var result = await StoreManager.SelectAsync (item);
+					if (result) {
+						AnatoliApp.GetInstance ().DefaultStore = item;
+						AnatoliApp.GetInstance ().ReplaceViewController (new FirstPageViewController ());
+					}
+				}
+                
+			}
+
+		}
+
+		public override nfloat GetHeightForRow (UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			return 70f;
+		}
+	}
 }
 
